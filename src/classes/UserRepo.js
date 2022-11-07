d27e00f8e3b5c0f5adc8eb45554ebdea5962b3ab
Anna Peterson <reveragb@gmail.com>
2022-11-07 15:32:53 -0700

    Deleted commented out code

--- src/classes/UserRepo.js
+++ src/classes/UserRepo.js
@@ -12,11 +12,6 @@ class UserRepo {
   let pantry = this.getUserInfo(userId).pantry
     return pantry
   }
-  // getPantry(userId) {
-  //   let userInfo = this.userCatalog.find((user) => userId === user.id);
-  //   return userInfo.pantry;
-  //   }
-
 }
 
 export default UserRepo;
